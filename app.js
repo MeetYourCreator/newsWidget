347b5af2a2d18baa3e1e199585c3f2e700b039ed
MeetYourCreator <ramizackshamir@icloud.com>
2020-12-13 21:11:19 -0500

    fetch returns response with data; init writing data to DOM

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
-const getTileData = async () => {
- 
-  const data = "https://api.taboola.com/1.2/json/apitestaccount/recommendations.get?app.type=web&app.apikey=7be65fc78e52c11727793f68b06d782cff9ede3c&source.id=%2Fdigiday-publishing-summit%2F&source.url=https%3A%2F%2Fblog.taboola.com%2Fdigiday-publishing-summit%2F&source.type=text&placement.organic-type=mix&placement.visible=true&placement.available=true&placement.rec-count=6&placement.name=Below%20Article%20Thumbnails&placement.thumbnail.width=640&placement.thumbnail.height=480&user.session=init";
-
-  try {
+const getTileData = () => {
   
-    const response = await axios.get(data);
-    let tiles = response.data.list;
-    console.table(tiles)
-    return tiles;
+  fetch("https://api.taboola.com/1.2/json/apitestaccount/recommendations.get?app.type=web&app.apikey=7be65fc78e52c11727793f68b06d782cff9ede3c&source.id=%2Fdigiday-publishing-summit%2F&source.url=https%3A%2F%2Fblog.taboola.com%2Fdigiday-publishing-summit%2F&source.type=text&placement.organic-type=mix&placement.visible=true&placement.available=true&placement.rec-count=6&placement.name=Below%20Article%20Thumbnails&placement.thumbnail.width=640&placement.thumbnail.height=480&user.session=init")
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.list);
+      document.getElementById('container').innerHTML = data.list;
 
-  } catch (error) {
-    console.log(`Error retrieving data: ${error}`) 
-  }
- 
+    })
 }
 
 getTileData();
 
-document.body.onload = addElement;
+// getTileData();
+›
+// document.body.onload = addElement;
 
-function addElement() {
-  // create a new div element 
-  const newDiv = document.createElement("div");
-  // and give it some content 
-  const newContent = document.createTextNode("Hi there and greetings!");
-  // add the text node to the newly created div
-  newDiv.appendChild(newContent);
-  // add the newly created element and its content into the DOM 
-  const currentDiv = document.getElementById("div1");
-  document.body.insertBefore(newDiv, currentDiv);
-}
+// function addElement() {
+//   // create a new div element 
+//   const newDiv = document.createElement("div");
+//   // and give it some content 
+//   const newContent = document.createTextNode("Hi there and greetings!");
+//   // add the text node to the newly created div
+//   newDiv.appendChild(newContent);
+//   // add the newly created element and its content into the DOM 
+//   const currentDiv = document.getElementById("div1");
+//   document.body.insertBefore(newDiv, currentDiv);
+// }
