9bcbad3fde976b10069ec759666b9f20a9605518
MeetYourCreator <ramizackshamir@icloud.com>
2020-12-14 18:27:52 -0500

    API call and renderData function

--- app.js
+++ app.js
@@ -1,47 +1,35 @@
-const getTileData = () => {
-  
-  fetch("https://api.taboola.com/1.2/json/apitestaccount/recommendations.get?app.type=web&app.apikey=7be65fc78e52c11727793f68b06d782cff9ede3c&source.id=%2Fdigiday-publishing-summit%2F&source.url=https%3A%2F%2Fblog.taboola.com%2Fdigiday-publishing-summit%2F&source.type=text&placement.organic-type=mix&placement.visible=true&placement.available=true&placement.rec-count=6&placement.name=Below%20Article%20Thumbnails&placement.thumbnail.width=640&placement.thumbnail.height=480&user.session=init")
-    .then(response => response.json())
-    .then(data => {
-      console.log(data.list);
-      const containerDiv = document.getElementById('container');
-      const tileData =
-      data.list.map(tile => {
-        console.log('yes')
-        document.createElement('div');
-        tile.innerHTML = `
+let tiles;
+
+//function that handles rendering data from API response
+const renderTiles = () => {
+
+  const containerDiv = document.getElementById('container');
+
+  tiles.map(tile => {
+    const section = document.createElement('section');
+    section.innerHTML = `
           <div class='tile-container'>
           <img
-          src=${tile.thumbnail[0].url}
-          class='images'>
+          src='${tile.thumbnail[0].url}'
+          class='thumbnail'>
           <p>${tile.name}</p>
           <p>${tile.categories[0]}</p>
           <p>${tile.branding}</p>
           </div>
         `
-       
-      })
-     
-      containerDiv.append(tileData)
-    })
-  
+    containerDiv.appendChild(section)
+  })
 }
 
-getTileData();
+//function that handles the API call
+const fetchTileData = async () => {
 
+  await fetch("https://api.taboola.com/1.2/json/apitestaccount/recommendations.get?app.type=web&app.apikey=7be65fc78e52c11727793f68b06d782cff9ede3c&source.id=%2Fdigiday-publishing-summit%2F&source.url=https%3A%2F%2Fblog.taboola.com%2Fdigiday-publishing-summit%2F&source.type=text&placement.organic-type=mix&placement.visible=true&placement.available=true&placement.rec-count=6&placement.name=Below%20Article%20Thumbnails&placement.thumbnail.width=640&placement.thumbnail.height=480&user.session=init")
+    .then(response => response.json())
+    .then(data => {
+      tiles = data.list
+      renderTiles()
+    })
+}
 
-// getTileData();
-
-// document.body.onload = addElement;
-
-// function addElement() {
-//   // create a new div element 
-//   const newDiv = document.createElement("div");
-//   // and give it some content 
-//   const newContent = document.createTextNode("Hi there and greetings!");
-//   // add the text node to the newly created div
-//   newDiv.appendChild(newContent);
-//   // add the newly created element and its content into the DOM 
-//   const currentDiv = document.getElementById("div1");
-//   document.body.insertBefore(newDiv, currentDiv);
-// }
+fetchTileData();
