292a9873ee4e3ea3ad563e8a05b4c45b91a21d8e
MeetYourCreator <ramizackshamir@icloud.com>
2020-12-13 21:27:38 -0500

    init .map

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const getTileData = () => {
     .then(response => response.json())
     .then(data => {
       console.log(data.list);
-      document.getElementById('container').innerHTML = data.list;
+      document.getElementById('container').innerHTML = data.list.map(tile => {
+        console.log('hello');
+        return tile
+      })
 
     })
 }
