7ea66015f2b8d947497e099528c8c4f2905f66b5
MeetYourCreator <ramizackshamir@icloud.com>
2020-12-18 12:04:03 -0500

    fixed API issue

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 let tiles;
 
+//function that handles the API call
+const fetchTileData = async () => {
+
+   await fetch("https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=aZamlLmHCkb79p0ejlBxscpBnBUFx4bb")
+    //this first function is where make the API call and get the response
+    .then(response => {
+      return response.json()
+    })
+    //this second function is where we get and render API data
+    .then(data => {
+      console.log(JSON.stringify(data.results));
+      tiles = data.results
+      console.table(`Console log tiles variable: ${tiles}`)
+      console.table(`Console log multimedia variable: ${tiles.multimedia}`)
+      renderTiles()
+    })
+}
+
 //function that handles rendering data from API response
 const renderTiles = () => {
 
@@ -22,22 +40,6 @@ const renderTiles = () => {
   })
 }
 
-//function that handles the API call
-const fetchTileData =  async () => {
 
-  await fetch("https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=RB52gKxhAGBULNNMAs5gXARnnimLsKMn")
-    //this first function is where make the API call and get the response
-    .then(response => {
-      return response.json()
-    })
-    //this second function is where we get and render API data
-    .then(data => {
-      console.log(JSON.stringify(data.results));
-      tiles= data.results
-      console.table(`Console log tiles variable: ${tiles}`)
-      
-      renderTiles()
-    })
-}
 
 fetchTileData();
